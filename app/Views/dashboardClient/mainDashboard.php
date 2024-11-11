b84790f781fdce1de3886e7901d4750dd96720eb
Mohamed Ouijjane <ouijjanemohamed2024@gmail.com>
2024-11-11 20:12:08 +0100

    adding welcomeDashboard view with its content

--- app/Views/dashboardClient/mainDashboard.php
+++ app/Views/dashboardClient/mainDashboard.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Tableau de Bord Admin</title>
+    <title>Tableau de bord Client</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <style>
--- app/Views/dashboardClient/welcomeDashboard.php
+++ app/Views/dashboardClient/welcomeDashboard.php
@@ -2,89 +2,130 @@
 
 <?= $this->section('content') ?>
 <style>
-    /* Styles généraux */
-    body {
-        font-family: 'Roboto', sans-serif;
-        background: linear-gradient(135deg, #f4f6f8, #d9e4ec);
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Inter", sans-serif;
     }
-
     .container {
-        max-width: 95%;
-       margin-top: 70px;
-      
+        margin-top: 10%;
     }
-
-    .card {
-        border-radius: 30px;
-        box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
-        border: none;
-        background: #ffffff;
-        border: 1px solid beige;
-        
-    }
-
-    .card-header {
-        background-color: transparent;
-        color: #007bff;
-        font-weight: bold;
-        text-align: center;
-        padding-bottom: 5px;
-        font-size: 1.5rem;
-    }
-
-    .form-label {
-        font-weight: 600;
-        color: #333;
-    }
-
-    .form-control {
-        border-radius: 10px;
-        border: 1px solid #ddd;
-        transition: border-color 0.3s, box-shadow 0.3s;
-        height: 40px;
-    }
-
-    .form-control:focus {
-        border-color: #007bff;
-        box-shadow: 0 0 5px rgba(0, 123, 255, 0.2);
-    }
-
-    /* Styles des icônes */
-    .form-group-icon {
-        position: relative;
-    }
-
-    .form-group-icon i {
-        position: absolute;
-        top: 73%;
-        left: 15px;
-        
-        transform: translateY(-50%);
-        color: #007bff;
-        font-size: 0.9rem;
-    }
-
-    /* Bouton */
-    .btn-success {
-        background: linear-gradient(135deg, #4CAF50, #36A764);
-        border: none;
-        padding: 10px 20px;
-        font-size: 1rem;
-        border-radius: 20px;
-        transition: background 0.3s, transform 0.2s;
+    .form-container {
+        font-family: "Inter", sans-serif;
         width: 100%;
-        color: #fff;
+        max-width: 400px;
+        padding: 10px;
+        border-radius: 5px;
+        text-align: center;
+        margin-right: 50%;
+    }
+    form {
+        width: 300%;
+        margin: 10%;
+        border-color: black;
+        border-radius: 5px;   
+        background-color: #fafafa ;
+        border: 1px solid;
+        border-color:#cccccc;
+    }
+    form h2 {
+        margin-bottom: 20px;
+        color: #333333;
     }
 
-    .btn-success:hover {
-        transform: translateY(-3px);
-        background: linear-gradient(135deg, #36A764, #4CAF50);
+    form label {
+        display: block;
+        text-align: left;
+        margin-bottom: 5px;
+        color: #555555;
+        font-weight: bold;
+    }
+
+    form input {
+        width: 60%;
+        padding: 8px;
+        margin-bottom: 20px;
+        border: 1px solid #cccccc;
+        border-radius: 4px;
+        font-size: 16px;
+    }
+
+    .title{
+        padding: 10px;
+        text-align: left;
+        position: relative;
+        font-size: 25px;
+
+    }
+    .titleContainer{
+        background-color: #f3f3f3;
+    }
+    .title::after {
+        content: "";
+        position: absolute;
+        bottom: 0; /* Position the line at the bottom of the padding */
+        left: 0;
+        width: 100%;
+        height: 1px; /* Thickness of the line */
+        background-color:  #115DFC; /* Color of the line */
+    }
+        @keyframes colorChange {
+    0%   {
+         color: blue;
+        }
+        100% {
+        color: red;
+        }}
+    .clignote {
+    animation: colorChange 1s infinite alternate;
+    text-align: center; 
+    font-family: system-ui;
+    font-size: 30px;
+    }
+    .spanDateExamen{
+        font-weight: bold; 
+        color: #666666;
+        margin-left: -241px;
+    }
+    .spanDateExamen2{
+        color: #A3A3A3;
+    }
+    .spanRef1{
+        color: #666666;
+        font-weight: bold;
+    }
+    .spanRef2{
+        color: #A3A3A3;
+    }
+    .spans h5{
+        margin-left: -30%;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
     }
 </style>
 
 <div class="container">
     <div class="card">
-        <div class="card-header">Bonjour chèr(e) Client, $ClientName!</div>
+        <div class="card-header"><h1>Bonjour chèr(e) Client, $ClientName!</h1></div>
+        <div class="clignote">Votre pré-inscription en ligne est envoyée!</div>
+        <div class="form-container">
+            <form class="form_container"> 
+                <div class="titleContainer">
+                    <p class="title">$Prufung B2</p>
+                </div>
+                <div class="spans">
+                <h5>
+                    <span class="spanRef1">Référence d'inscription:</span>
+                    <span class="spanRef2">EK3663366552101100</span>
+                </h5>
+                <h5 class="dateE" >
+                    <span class="spanDateExamen">Date d'examen:</span>
+                    <span class="spanDateExamen2">$date</span>
+                </h5>
+
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 <?= $this->endSection() ?>
